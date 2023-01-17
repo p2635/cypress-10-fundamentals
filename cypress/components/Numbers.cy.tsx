9373d8ceeba51017a9088c56a4e22ebed6a392f5
p2635 <4522927+p2635@users.noreply.github.com>
2023-01-17 17:01:29 +0000

    new test to click on a number

--- cypress/components/Numbers.cy.tsx
+++ cypress/components/Numbers.cy.tsx
@@ -10,5 +10,19 @@ describe('Numbers', { viewportHeight: 1000, viewportWidth: 1000 }, () => {
         </section>
       </div>
     )
+    cy.get('.status__number').should('have.length', 9)
+  })
+
+  it('Click on a number', () => {
+    cy.mount(
+      <div className='innercontainer'>
+        <section className='status'>
+          <Numbers onClickNumber={cy.stub().as('clickedNumber')} />
+        </section>
+      </div>
+    )
+    cy.get('.status__number').should('have.length', 9)
+    cy.contains('.status__number', '1').click()
+    cy.get('@clickedNumber').should('have.been.calledOnce', '1')
   })
 })
